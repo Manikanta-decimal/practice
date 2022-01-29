b1dccc5d49a5ecc0a6c569e13a0442a5bf98793d
Manikanta <tangi.manikanta@decimal.co.in>
2022-01-29 12:36:02 +0530

    recent

--- sample/src/sample/Sample.java
+++ sample/src/sample/Sample.java
@@ -1,5 +1,15 @@
 package sample;
-
+import java.util.Scanner;
 public class Sample {
-
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Are you a failure");
+		String n = sc.nextLine();
+		if(n=="yes") {
+			System.out.println(true);
+		}
+		else {
+			System.out.print("however you are a failure!!!");
+		}
+	}
 }
